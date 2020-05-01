645f8b298c4d1f4e6fd559d4ca36ecde1357065c
Duo <duochen@yahoo.com>
2020-05-01 15:02:29 -0500

    check in code

---
+++ Lecture09/binary_serach.py
@@ -0,0 +1,25 @@
+# Binary Search
+
+def binary_search(list, value):
+    size = len(list) - 1
+    start = 0
+    end = size
+    # Find the middle most value
+    while start <= end:
+        mid = (start + end) // 2
+        if list[mid] == value:
+            return mid
+        # Compare the value the middle most value
+        if value > list[mid]:
+            start = mid + 1
+        else :
+            end = mid - 1
+    if start > end:
+        return None
+
+##########################################
+
+list = [ 2 , 7 , 19 , 34 , 53 , 72 ]
+
+print (binary_search(list, 72))
+print (binary_search(list, 11))
