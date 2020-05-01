b3baf245463dc817821454c87962a3a3b2ddee8c
Duo <duochen@yahoo.com>
2020-05-01 11:12:32 -0500

    check in code

---
+++ Lecture10/graph_v2.py
@@ -0,0 +1,22 @@
+class graph:
+    def __init__(self, dict = None):
+        if dict is None:
+            dict = []
+        self.dict = dict
+    
+    def get_vertices(self):
+        return list(self.dict.keys())
+
+
+#####################################
+
+dict = {
+    "a": ["b", "c"],
+    "b" : [ "a" , "d" ],
+    "c" : [ "a" , "d" ],
+    "d" : [ "e" ],
+    "e" : [ "d" ]
+}
+
+g = graph(dict)
+print(g.get_vertices())
