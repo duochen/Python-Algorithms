49ff8e3c6fd0914288b7e6f30c81fa0a5d4758dc
Duo <duochen@yahoo.com>
2020-05-08 14:59:49 -0500

    check in code

---
+++ Lecture07/hashf.py
@@ -0,0 +1,10 @@
+def myhash(s):
+    mult = 1
+    hv = 0
+    for ch in s:
+        hv += mult * ord(ch)
+        mult += 1
+    return hv
+
+for item in ('hello world', 'world hello', 'gello xorld', 'ad', 'ga'):
+    print("{}: {}".format(item, myhash(item)))
