828a8f23595c1f1e6a81b0ec29dca569bab32c70
Duo <duochen@yahoo.com>
2020-05-20 23:49:09 -0500

    check in code

---
+++ Lecture01/Labs/Lab01/rec_cum_challenge.py
@@ -0,0 +1,12 @@
+# Write a recursive function which takes an integer and computes the cumulative sum of 0 to that integer
+# For example, if n=4 , return 4+3+2+1+0, which is 10.
+# This problem is very similar to the factorial problem presented during the introduction to recursion. 
+# Remember, always think of what the base case will look like. 
+# In this case, we have a base case of n =0 (Note, you could have also designed the cut off to be 1).
+# In this case, we have: n + (n-1) + (n-2) + .... + 0
+
+def rec_sum(n):
+    pass
+
+result = rec_sum(4)   
+print(result)           # 10
