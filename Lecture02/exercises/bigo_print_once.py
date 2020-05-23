63c885b163cacf768ce95d38166fbc9540d8a301
Duo <duochen@yahoo.com>
2020-05-23 13:37:08 -0500

    check in code

---
+++ Lecture02/exercises/bigo_print_once.py
@@ -0,0 +1,8 @@
+def print_once(lst):
+    # Prints all items once
+    for val in lst:
+        print(val)
+
+##################################
+
+print_once([0,1,2,3])
---
+++ Lecture02/exercises/bigo_print_three_times.py
@@ -0,0 +1,15 @@
+def print_3(lst):
+    # Prints all items three times
+
+    for val in lst:
+        print(val)
+
+    for val in lst:
+        print(val)
+
+    for val in lst:
+        print(val)
+
+#################################
+
+print_3([0,1,2,3])
