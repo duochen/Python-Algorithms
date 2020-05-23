2382a8b27f6c3626e8afddb8dbcc80b12932d080
Duo <duochen@yahoo.com>
2020-05-23 11:26:20 -0500

    check in code

---
+++ Lecture02/exercises/bigo_constant.py
@@ -0,0 +1,7 @@
+def func_constant(values):
+    # Prints first item in a list of values
+    print(values[0])
+
+################################
+
+func_constant([1,2,3])
---
+++ Lecture02/exercises/bigo_linear.py
@@ -0,0 +1,8 @@
+def func_lin(list):
+    # Takes in list and prints out all values
+    for val in list:
+        print(val)
+
+################################
+
+func_lin([1,2,3])
