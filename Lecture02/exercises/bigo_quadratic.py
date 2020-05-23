bdbfa9fe57a094347f521aec9d21d410b433623a
Duo <duochen@yahoo.com>
2020-05-23 11:29:29 -0500

    check in code

---
+++ Lecture02/exercises/bigo_quadratic.py
@@ -0,0 +1,9 @@
+def func_quad(list):
+    # Prints pairs for every item in list
+    for item1 in list:
+        for item2 in list:
+            print(item1, item2)
+
+###########################################
+
+func_quad([0,1,2,3])
