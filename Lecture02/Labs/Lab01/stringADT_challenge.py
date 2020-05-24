721da6462a4edd7f866a62ece601b2e3eb045db8
Duo <duochen@yahoo.com>
2020-05-24 14:49:46 -0500

    check in code

---
+++ Lecture02/Labs/Lab01/stringADT_challenge.py
@@ -0,0 +1,23 @@
+class stringADT:
+    def __init__(self, str):
+        pass
+
+    def upper(self):
+        pass
+
+    def lower(self):
+        pass
+
+    def length(self):
+        pass
+
+    def display(self):
+        pass
+
+##########################################
+
+s = stringADT("Hello World")
+print(s.upper())
+print(s.lower())
+print(s.length())
+s.display()
