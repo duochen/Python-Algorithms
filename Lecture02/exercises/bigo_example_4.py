28e8dc200c6d69925d535bee75b6819d13239a49
Duo <duochen@yahoo.com>
2020-05-24 18:21:08 -0500

    check in code

---
+++ Lecture02/exercises/bigo_example_4.py
@@ -0,0 +1,9 @@
+def someAlgorithm(n):
+   Assignment
+
+   i = n-1
+   while i >= 1:
+      Arithmetic operation
+      i/=2
+      
+    Output
---
+++ Lecture02/exercises/bigo_example_5.py
@@ -0,0 +1,10 @@
+def someAlgorithm(n):
+   Input
+
+   for i in range(n):
+      Assignment
+      for j in range(n):
+         Comparison
+
+   Arithmetic operation   
+   Output
