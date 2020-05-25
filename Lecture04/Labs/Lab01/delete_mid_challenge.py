7278062cc5cfc6703ee14da2e26e7b4f46b74030
Duo <duochen@yahoo.com>
2020-05-24 23:29:07 -0500

    check in code

---
+++ Lecture04/Labs/Lab01/delete_mid_challenge.py
@@ -0,0 +1,7 @@
+from LinkedList import LinkedList
+
+class MyLinkedList(LinkedList):
+    
+    def delete_node(self, node):
+        # TODO: Implement me
+        pass
---
+++ Lecture04/Labs/Lab01/delete_mid_solution.py
@@ -0,0 +1,12 @@
+from LinkedList import LinkedList
+
+class MyLinkedList(LinkedList):
+    
+    def delete_node(self, node):
+        if node is None:
+            return
+        if node.next is None:
+            node.data = None
+        else:
+            node.data = node.next.data
+            node.next = node.next.next
