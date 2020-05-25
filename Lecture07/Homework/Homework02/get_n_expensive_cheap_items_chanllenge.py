22cc279e74687a0d1e0594054a71b4933748458b
Duo <duochen@yahoo.com>
2020-05-25 17:24:12 -0500

    check in code

---
+++ Lecture07/Homework/Homework02/get_n_expensive_cheap_items_chanllenge.py
@@ -0,0 +1,11 @@
+import heapq
+from pprint import pprint
+
+items = [
+    {'name': 'Item-1', 'price': 101.1},
+    {'name': 'Item-2', 'price': 555.22},
+    {'name': 'Item-3', 'price': 45.09},
+    {'name': 'Item-4', 'price': 22.75},
+    {'name': 'Item-5', 'price': 16.30},
+    {'name': 'Item-6', 'price': 110.65}
+]
