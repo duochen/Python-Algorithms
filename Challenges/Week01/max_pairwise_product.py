e443ec07d572b2e2866d9f84fa6fd7ce62ca9dcf
Duo <duochen@yahoo.com>
2020-05-30 17:31:01 -0500

    check in code

--- Challenges/Week01/max_pairwise_product.py
+++ Challenges/Week01/max_pairwise_product.py
@@ -1,19 +1,33 @@
-def max_pairwise_product(numbers):
+import time
+
+def method1(numbers):
+    start = time.perf_counter()
     n = len(numbers)
     #just find biggest two numbers
     num1 = max(numbers)
     numbers.remove(num1)
     num2 = max(numbers)
-    return num1*num2
+    result = num1 * num2
+    end = time.perf_counter()
+    print(end-start)
+    return result
+
+def method2(numbers):
+    start = time.perf_counter()
+    n = len(numbers)
+    max_product = 0
+    for first in range(n):
+        for second in range(first + 1, n):
+            max_product = max(max_product,
+                numbers[first] * numbers[second])
+    end = time.perf_counter()
+    print(end-start)    
+    return max_product
+
+def max_pairwise_product(numbers):
+    # method1(numbers)
+    method2(numbers)
     
-#     max_product = 0
-#     for first in range(n):
-#         for second in range(first + 1, n):
-#             max_product = max(max_product,
-#                 numbers[first] * numbers[second])
-
-#     return max_product
-
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
