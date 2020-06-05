0f81d18d0dedd8265c09baea2918fbbc1ad02148
Duo <duochen@yahoo.com>
2020-06-05 10:48:35 -0500

    check in code

---
+++ Lecture01/Quiz/quiz_01.py
@@ -0,0 +1,9 @@
+def my_recursive_function(n):
+    if n == 0:
+        return
+    print(n)
+    my_recursive_function(n-1)
+
+my_recursive_function(10)
+
+
---
+++ Lecture09/Homework/Homework01/FirstRepeatedElementAmongRepeatedElementsWithHash.py
@@ -0,0 +1,22 @@
+def FirstRepeatedElementAmongRepeatedElementsWithHash(A):
+    table = {}  # hash
+    max = 0
+    for element in A:
+        if element in table and table[element] == 1:
+            table[element] = -2
+        elif element in table and table[element] < 0:
+            table[element] -= 1			
+        elif element != " ":
+            table[element] = 1
+        else:
+            table[element] = 0
+
+    for element in A:
+        if table[element] < max:
+            max = table[element]
+            maxRepeatedElement = element
+
+    print(maxRepeatedElement, "repeated for ", abs(max), " times")
+ 
+A = [3, 2, 1, 1, 2, 1, 2, 5, 5]
+FirstRepeatedElementAmongRepeatedElementsWithHash(A)
