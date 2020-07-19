de8d6c7557b5ff01409676a0e69cbdb87e8aba3e
Duo <duochen@yahoo.com>
2020-07-19 08:53:16 -0500

    check in code

--- Lecture08/Exercises/ShellSort.py
+++ Lecture08/Exercises/ShellSort.py
@@ -1,16 +1,28 @@
-def shellsort(list):
-    gap = len(list) // 2
-    while gap > 0:
-        for i in range(gap, len(list)):
-            temp = list [i]
-            j = i
-            # Sort the sub list for this gap
-            while j >= gap and list [j - gap] > temp:
-                list [j] = list [j - gap]
-                j = j - gap
-            list [j] = temp
-        # Reduce the gap for the next element
-        gap = gap // 2
+def shellsort(arr): 
+    # Start with a big gap, then reduce the gap 
+    n = len(arr) 
+    gap = int(n/2)
+  
+    # Do a gapped insertion sort for this gap size. 
+    # The first gap elements a[0..gap-1] are already in gapped  
+    # order keep adding one more element until the entire array 
+    # is gap sorted 
+    while gap > 0: 
+        for i in range(gap,n): 
+            # add a[i] to the elements that have been gap sorted 
+            # save a[i] in temp and make a hole at position i 
+            temp = arr[i] 
+  
+            # shift earlier gap-sorted elements up until the correct 
+            # location for a[i] is found 
+            j = i 
+            while  j >= gap and arr[j-gap] >temp: 
+                arr[j] = arr[j-gap] 
+                j = j - gap 
+  
+            # put temp (the original a[i]) in its correct location 
+            arr[j] = temp 
+        gap = int(gap/2)
 
 ###################################################
 
