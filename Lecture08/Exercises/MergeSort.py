a57733e41db0c432d26d3772508264708e7f6b3f
Duo <duochen@yahoo.com>
2020-07-19 09:23:59 -0500

    check in code

--- Lecture08/Exercises/MergeSort.py
+++ Lecture08/Exercises/MergeSort.py
@@ -1,36 +1,61 @@
-def mergesort(list):
-    if len(list) > 1:
-        mid = len(list) // 2
-        left = list[:mid]
-        right = list[mid:]
+def merge(left, right):
+    # If the first array is empty, then nothing needs
+    # to be merged, and you can return the second array as the result
+    if len(left) == 0:
+        return right
 
-        mergesort(left)
-        mergesort(right)
+    # If the second array is empty, then nothing needs
+    # to be merged, and you can return the first array as the result
+    if len(right) == 0:
+        return left
 
-        i,j,k = 0,0,0
+    result = []
+    index_left = index_right = 0
 
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                list[k] = left[i]
-                i = i + 1
-            else:
-                list[k] = right[j]
-                j = j + 1
-            k = k + 1
+    # Now go through both arrays until all the elements
+    # make it into the resultant array
+    while len(result) < len(left) + len(right):
+        # The elements need to be sorted to add them to the
+        # resultant array, so you need to decide whether to get
+        # the next element from the first or the second array
+        if left[index_left] <= right[index_right]:
+            result.append(left[index_left])
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
 
-        while i < len(left):
-            list[k] = left[i]
-            i = i + 1
-            k = k + 1
+        # If you reach the end of either array, then you can
+        # add the remaining elements from the other array to
+        # the result and break the loop
+        if index_right == len(right):
+            result += left[index_left:]
+            break
 
-        while j < len(right):
-            list[k] = right[j]
-            j = j + 1
-            k = k + 1
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+
+    return result
+
+def mergesort(array):
+    # If the input array contains fewer than two elements,
+    # then return it as the result of the function
+    if len(array) < 2:
+        return array
+
+    midpoint = len(array) // 2
+
+    # Sort the array by recursively splitting the input
+    # into two equal halves, sorting each half and merging them
+    # together into the final result
+    return merge(
+        left=mergesort(array[:midpoint]),
+        right=mergesort(array[midpoint:]))    
 
 ###################################################
 
 list = [84, 21, 96, 15, 47]
 print('Original list: ', list)
-mergesort(list)
-print('Sorted list: ', list)
+result = mergesort(list)
+print('Sorted list: ', result)        
