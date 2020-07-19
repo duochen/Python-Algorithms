41ac517ae88b0049989cb098ded4b1d52b326931
Duo <duochen@yahoo.com>
2020-07-19 09:05:05 -0500

    check in code

--- Lecture08/Exercises/HeapSort.py
+++ Lecture08/Exercises/HeapSort.py
@@ -1,14 +1,43 @@
-from ArrayHeap import ArrayHeap
+# To heapify subtree rooted at index i. 
+# n is size of heap 
+def heapify(arr, n, i): 
+    largest = i # Initialize largest as root 
+    l = 2 * i + 1     # left = 2*i + 1 
+    r = 2 * i + 2     # right = 2*i + 2 
+  
+    # See if left child of root exists and is 
+    # greater than root 
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+  
+    # See if right child of root exists and is 
+    # greater than root 
+    if r < n and arr[largest] < arr[r]: 
+        largest = r 
+  
+    # Change root, if needed 
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i] # swap 
+  
+        # Heapify the root. 
+        heapify(arr, n, largest) 
+  
+# The main function to sort an array of given size 
+def heapsort(arr): 
+    n = len(arr) 
+  
+    # Build a maxheap. 
+    for i in range(n//2 - 1, -1, -1): 
+        heapify(arr, n, i) 
+  
+    # One by one extract elements 
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i] # swap 
+        heapify(arr, i, 0) 
+  
+###################################################
 
-#######################################################################
-
-h = ArrayHeap()
-h.insert(25)
-h.insert(14)
-h.insert(2)
-h.insert(20)
-h.insert(10)
-h.insert(12)
-print(h.data)
-for i in range(h.currentsize):
-    print(h.deletemin(), end = ' , ')
+list = [84, 21, 96, 15, 47]
+print('Original list: ', list)
+heapsort(list)
+print('Sorted list: ', list)
