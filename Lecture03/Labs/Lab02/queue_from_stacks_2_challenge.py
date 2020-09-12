356a26e697808c34a89655dba80d0580bbd5381d
Duo <duochen@yahoo.com>
2020-09-11 22:59:07 -0500

    check in code

---
+++ Lecture03/Labs/Lab02/queue_from_stacks_2_challenge.py
@@ -0,0 +1,22 @@
+class MyQueue:
+    def __init__(self):
+        pass
+
+    def push(self, x):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def empty(self):
+        pass
+    
+queue = MyQueue()
+queue.push(1)
+queue.push(2)  
+print(queue.peek())    # Return 1
+print(queue.pop())     # Return 1
+print(queue.empty())   # Return False       
