7162d2e28d2e5aeec4e6ed23705c71937727fb03
Duo <duochen@yahoo.com>
2021-01-01 13:33:28 -0600

    check in code

---
+++ Lecture09/Homework/Homework02/079 - start.py
@@ -0,0 +1,8 @@
+class Solution:
+    def exist(self, board, word):
+        pass
+    
+s = Solution()
+print(s.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED"))
+print(s.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE"))
+print(s.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB"))
