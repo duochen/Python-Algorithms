097f2154e11c5db5da55182a7a77fe3dc5609a85
Duo <duochen@yahoo.com>
2021-01-01 13:23:51 -0600

    check in code

--- Lecture08/Labs/Lab03/088-3.py
+++ Lecture08/Labs/Lab03/088-3.py
@@ -1,12 +1,5 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: void Do not return anything, modify nums1 in-place instead.
-        """
         # two get pointers for nums1 and nums2
         p1 = m - 1
         p2 = n - 1
