aefca8f6bf080754c1f9bc5f80a5a52634cd1108
Duo <duochen@yahoo.com>
2021-05-01 16:02:38 -0500

    check in code

---
+++ Lecture04/Labs/Lab01/delete_mid_challenge2.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.next = next
+        self.data = data
+        
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_first(self, data):
+        if data is None:
+            return None
+        
+        node = Node(data, self.head)
+        self.head = node
+        return node        
+
+    def delete_node(self, node):
+        pass
+
+    def display(self):
+        node = self.head
+        while node is not None:
+            print(node.data)
+            node = node.next
+
+list = MyLinkedList()
+list.add_first(2)
+list.add_first(3)
+list.add_first(4)
+list.add_first(1)
+print("Before deletion")
+list.display()
+print("The node needs to be deleted: ", list.head.next.next.data)
+list.delete_node(list.head.next.next)
+print("After deletion")
+list.display()
