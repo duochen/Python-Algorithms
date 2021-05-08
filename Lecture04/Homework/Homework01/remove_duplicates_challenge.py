2e370b77d04b3d95cc37eb74a4bc97c2a8aaab4d
Duo <duochen@yahoo.com>
2021-05-08 08:50:54 -0500

    check in code

--- Lecture04/Homework/Homework01/remove_duplicates_challenge.py
+++ Lecture04/Homework/Homework01/remove_duplicates_challenge.py
@@ -1,7 +1,41 @@
-from LinkedList import LinkedList
+class Node:
+    
+    def __init__(self, data, next=None):
+        self.next = next
+        self.data = data
+
+class LinkedList:
+
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert_to_front(self, data):
+        if data is None:
+            return None
+        node = Node(data, self.head)
+        self.head = node
+        return node
+
+    def get_all_data(self):
+        data = []
+        curr_node = self.head
+        while curr_node is not None:
+            data.append(curr_node.data)
+            curr_node = curr_node.next
+        return data
 
 class MyLinkedList(LinkedList):
     
     def remove_dupes(self):
-        # TODO: Implement me
         pass
+    
+linked_list = MyLinkedList()
+linked_list.insert_to_front(1)
+linked_list.insert_to_front(1)
+linked_list.insert_to_front(3)
+linked_list.insert_to_front(2)
+linked_list.insert_to_front(3)
+linked_list.insert_to_front(1)
+linked_list.insert_to_front(1)
+linked_list.remove_dupes()
+print(linked_list.get_all_data() == [1, 3, 2])
--- Lecture04/Homework/Homework01/remove_duplicates_solution.py
+++ Lecture04/Homework/Homework01/remove_duplicates_solution.py
@@ -1,4 +1,28 @@
-from LinkedList import LinkedList
+class Node:
+    
+    def __init__(self, data, next=None):
+        self.next = next
+        self.data = data
+
+class LinkedList:
+
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert_to_front(self, data):
+        if data is None:
+            return None
+        node = Node(data, self.head)
+        self.head = node
+        return node
+
+    def get_all_data(self):
+        data = []
+        curr_node = self.head
+        while curr_node is not None:
+            data.append(curr_node.data)
+            curr_node = curr_node.next
+        return data
 
 class MyLinkedList(LinkedList):
     
@@ -15,3 +39,14 @@ class MyLinkedList(LinkedList):
             else:
                 prev.next = node.next
                 node = node.next
+
+linked_list = MyLinkedList()
+linked_list.insert_to_front(1)
+linked_list.insert_to_front(1)
+linked_list.insert_to_front(3)
+linked_list.insert_to_front(2)
+linked_list.insert_to_front(3)
+linked_list.insert_to_front(1)
+linked_list.insert_to_front(1)
+linked_list.remove_dupes()
+print(linked_list.get_all_data() == [1, 3, 2])
