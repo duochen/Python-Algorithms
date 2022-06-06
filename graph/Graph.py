abae72ad17261ebee3407546f143a11ef325aa78
duochen <duochen@yahoo.com>
2022-06-05 20:11:47 -0500

    check in code

--- graph/Graph.py
+++ graph/Graph.py
@@ -7,15 +7,28 @@ class Graph:
     def addEdge(self, vertex, edge):
         self.gdict[vertex].append(edge)
 
+    def bfs(self, vertex):
+        visited = [vertex]
+        queue = [vertex]
+        while queue:
+            deVertex = queue.pop(0)
+            print(deVertex)
+            for adjacentVertex in self.gdict[deVertex]:
+                if adjacentVertex not in visited:
+                    visited.append(adjacentVertex)
+                    queue.append(adjacentVertex)
+
 customDict = {
     "a": ["b", "c"],
-    "b": ["a", "d", "e"],
-    "c": ["a", "e"],
-    "d": ["b", "e", "f"],
-    "e": ["b", "d", "f"],
-    "f": ["d", "e"]
+    "b": ["a", "d", "g"],
+    "c": ["a", "d", "e"],
+    "d": ["b", "c", "f"],
+    "e": ["c", "f"],
+    "f": ["d", "e", "g"],
+    "g": ["b", "f"]
 }
 
 graph = Graph(customDict)
-graph.addEdge("e", "c")
-print(graph.gdict["e"])
+# graph.addEdge("e", "c")
+# print(graph.gdict["e"])
+graph.bfs('a')
