590c41530e4653e9adb7ce569de7d2fdfd8e6c69
duochen <duochen@yahoo.com>
2022-06-22 20:54:29 -0500

    check in code

--- graph/Graph.py
+++ graph/Graph.py
@@ -18,6 +18,22 @@ class Graph:
                     visited.append(adjacentVertex)
                     queue.append(adjacentVertex)
 
+    def dfs(self, vertex):
+        visited = [vertex]
+        stack = [vertex]
+        while stack:
+            popVertex = stack.pop()
+            print(popVertex)
+            for adjacentVertex in self.gdict[popVertex]:
+                if adjacentVertex not in visited:
+                    visited.append(adjacentVertex)
+                    stack.append(adjacentVertex)
+
+    def topologicalSortUtil(self, v, visited, stack):
+        visited.append(v)
+
+        for i in self.g
+
 customDict = {
     "a": ["b", "c"],
     "b": ["a", "d", "g"],
@@ -31,4 +47,5 @@ customDict = {
 graph = Graph(customDict)
 # graph.addEdge("e", "c")
 # print(graph.gdict["e"])
+graph.dfs('a')
 graph.bfs('a')
